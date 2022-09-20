73b3aa0da405b12a6bc74a8e0dc9657ab87ea839
gedexo <mkswathisuresh@gmail.com>
2022-09-20 17:16:07 +0530

    Template setting and dashboard icon

--- web/urls.py
+++ web/urls.py
@@ -10,5 +10,20 @@ urlpatterns =[
     path('forgot-password/',views.forgot_password,name='forgot_password'),
     path('profile/',views.profile,name='profile'),
     path('settings/',views.settings,name='settings'),
-    path('dashboard/',views.index,name='index')
+    path('dashboard/',views.index,name='index'),
+    path('service/',views.service,name='service'),
+    path('generate-poster/',views.generatePoster,name='generatePoster'),
+    path('generate-bill/',views.generateBill,name='generateBill'),
+    path('generate-form/',views.generateForms,name='generateForms'),
+    path('documents/',views.documents,name='documents'),
+    path('softwares/',views.software,name='software'),
+    path('tools/',views.tools,name='tools'),
+    path('marketing-tip/',views.marketingTip,name='marketingTip'),
+    path('other-ideas/',views.otherIdea,name='otherIdea'),
+    path('agency-portal/',views.agencyPortal,name='agencyPortal'),
+    path('back-office-services/',views.backOfficeServices,name='backOfficeServices'),
+    path('bonus/',views.bonus,name='bonus'),
+    path('support/',views.support,name='support'),
+
+
 ]
--- web/views.py
+++ web/views.py
@@ -40,3 +40,94 @@ def index(request):
 
     }
     return render(request,'web/index.html',context)
+
+
+def service(request):
+    context = {
+
+    }
+    return render(request,'web/service.html',context)
+
+
+def generatePoster(request):
+    context = {
+
+    }
+    return render(request,'web/generate-poster.html',context)
+
+
+def generateBill(request):
+    context = {
+
+    }
+    return render(request,'web/generate-bill.html',context)
+
+
+def generateForms(request):
+    context = {
+
+    }
+    return render(request,'web/generate-form.html',context)
+
+
+def documents(request):
+    context = {
+
+    }
+    return render(request,'web/documents.html',context)
+
+
+def software(request):
+    context = {
+
+    }
+    return render(request,'web/softwares.html',context)
+
+
+def tools(request):
+    context = {
+
+    }
+    return render(request,'web/tools.html',context)
+
+
+def marketingTip(request):
+    context = {
+
+    }
+    return render(request,'web/marketing-tip.html',context)
+
+
+def otherIdea(request):
+    context = {
+
+    }
+    return render(request,'web/other-ideas.html',context)
+
+
+def agencyPortal(request):
+    context = {
+
+    }
+    return render(request,'web/agency-portal.html',context)
+
+
+def backOfficeServices(request):
+    context = {
+
+    }
+    return render(request,'web/back-office-services.html',context)
+
+
+def bonus(request):
+    context = {
+
+    }
+    return render(request,'web/bonus.html',context)
+
+
+def support(request):
+    context = {
+
+    }
+    return render(request,'web/support.html',context)
