851c3c84890ba0032705ed842460c08ae24ebf98
gedexo <mkswathisuresh@gmail.com>
2022-09-21 17:19:07 +0530

    Dashboard and service

---
+++ static/web/js/main.js
@@ -0,0 +1,11 @@
+
+  $(function() {
+  
+    // Initial hide div.
+    $('#subservice-list').hide();
+    
+    $('[id="serice-head"]').click(function() {
+      // Then show div on click.
+      $('#subservice-list').show();
+    })
+  })
--- web/views.py
+++ web/views.py
@@ -23,7 +23,7 @@ def forgot_password(request):
 
 def profile(request):
     context = {
-
+        "is_profile":True,
     }
     return render(request,'web/profile.html',context)
 
@@ -37,97 +37,97 @@ def settings(request):
 
 def index(request):
     context = {
-
+        "is_index":True,
     }
     return render(request,'web/index.html',context)
 
 
 def service(request):
     context = {
-
+        "is_service":True,
     }
     return render(request,'web/service.html',context)
 
 
 def generatePoster(request):
     context = {
-
+        "is_poster":True,
     }
     return render(request,'web/generate-poster.html',context)
 
 
 def generateBill(request):
     context = {
-
+        "is_bill":True,
     }
     return render(request,'web/generate-bill.html',context)
 
 
 def generateForms(request):
     context = {
-
+        "is_form":True,
     }
     return render(request,'web/generate-form.html',context)
 
 
 def documents(request):
     context = {
-
+        "is_document":True,
     }
     return render(request,'web/documents.html',context)
 
 
 def software(request):
     context = {
-
+        "is_software":True,
     }
     return render(request,'web/softwares.html',context)
 
 
 def tools(request):
     context = {
-
+        "is_tool":True,
     }
     return render(request,'web/tools.html',context)
 
 
 def marketingTip(request):
     context = {
-
+        "is_tip":True,
     }
     return render(request,'web/marketing-tip.html',context)
 
 
 def otherIdea(request):
     context = {
-
+        "is_idea":True,
     }
     return render(request,'web/other-ideas.html',context)
 
 
 def agencyPortal(request):
     context = {
-
+        "is_portal":True,
     }
     return render(request,'web/agency-portal.html',context)
 
 
 def backOfficeServices(request):
     context = {
-
+        "is_backservice":True,
     }
     return render(request,'web/back-office-services.html',context)
 
 
 def bonus(request):
     context = {
-
+        "is_bonus":True,
     }
     return render(request,'web/bonus.html',context)
 
 
 def support(request):
     context = {
-
+        "is_support":True,
     }
     return render(request,'web/support.html',context)
